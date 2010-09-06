00d7f4cafc194b0336e9ff311428b49c47976a81
Quentin Stievenart <acieroid@awesom.eu>
2010-09-06 22:53:23 +0200

    adding host management in statping.py

--- statping.py
+++ statping.py
@@ -1,15 +1,23 @@
 #!/usr/bin/env python
 from os import system
 from cgi import FieldStorage, escape
+from re import sub
 
+admin = True                    # do we auhorize admin stuff or not ?
 generator = './rendergraph'
 img_path = ''
+conf_path = ''
 
 ### "system" functions
 def get_hosts():
-  f = open('hosts.cnf')
+  f = open(conf_path + 'hosts.cnf')
   for line in f:
-    yield line[:-1]
+    yield sub(r'([^\r\n]+)\r?\n', r'\1', line)
+  f.close()
+
+def save_hosts(content):
+  f = open(conf_path + 'hosts.cnf', 'w')
+  f.write(content)
   f.close()
 
 def gen_graph(host):
@@ -27,7 +35,7 @@ def show_host(params):
   # TODO: change the step etc.
   host = params.getvalue('host')
   html = '<h1>Statping for ' + host + '</h1>'
-
+  
   if not host in get_hosts():
     return '<p>No such host</p>'
   
@@ -35,6 +43,19 @@ def show_host(params):
   html += '<img src="' + img_path + host + '.png" alt="' + host + '"/>'
   return html
 
+def manage_hosts(params):
+  html = '<h1>Manage hosts</h1>'
+  if 'new_hosts' in params:
+    save_hosts(params.getvalue('new_hosts'))
+    return html + '<p>Hosts saved<br/><a href="">back</a></p>'
+  
+  html += '<form method="post" action="/?manage=t" enctype="multipart/form-data">'
+  html += '<textarea name="new_hosts" rows="20" cols="80">\n'
+  for host in get_hosts():
+    html += host + '\n'
+  html += '</textarea><br/>'
+  html += '<input type="submit" value="update" />'
+  return html
 
 ### app
 def statping(environ, start_response):
@@ -42,8 +63,13 @@ def statping(environ, start_response):
   start_response('200 OK', [('Content-Type', 'text/html'),
                             ('charset', 'utf-8')])
   body = ''
+  if admin:
+    body += '<a href="/?manage=t">manage</a> - '
+  body += '<a href="/">list</a><br/>'
   if 'host' in params:
     body += show_host(params)
+  elif admin and 'manage' in params or 'new_hosts' in params:
+    body += manage_hosts(params)
   else:
     body += list_hosts(params)
   return body
