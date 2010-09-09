ff5391bbac739329ea7e69377ef1535ac885665b
David Hymonnet <bougieskater@gmail.com>
2010-09-09 14:54:35 +0200

    Increase GUI experience <3

--- confs/config.py
+++ confs/config.py
@@ -4,7 +4,12 @@
 # config.py : configuration variables
 #
 
+from time import localtime, strftime
+
 data_dir = ""
 graph_dir = ""
 
 admin = True
+
+default_step = 86400
+default_begin = strftime("%m/%d/%Y %H:%M", localtime())
--- public/statping.py
+++ public/statping.py
@@ -12,24 +12,45 @@ from confs.config import *
 
 path = ''
 
-### pages
+#
+# list_hosts : return a html list wich contains hosts list
+#
 def list_hosts(params):
-  html = '<h1>Hosts</h1><ul>'
+  html = '<ul>'
+  urlhost = params.getvalue('host')
+
   for host in get_hosts():
-    html += '<li><a href="' + path + '?host=' + host + '">' + host + '</a></li>'
+    if host == urlhost:
+      html += '<li>' + host + '</li>'
+    else:
+      html += '<li><a href="' + path + '?host=' + host + '">' + host + '</a></li>'
+
   html += '</ul>'
+
   return html
 
-def show_host(params):
-  host = params.getvalue('host')
+#
+# gen_menu : generate the side menu
+#
+def gen_menu(params):
+  html = '<div class="menu">';
+
+  html += list_hosts(params)
+
+  html += '</div>';
+
+  return html
+
+#
+# gen_renderform : configure and generate render host form
+#
+def gen_renderform(params):
+  html = '<div class="renderform">'
+
+  host = params.getvalue('host') or 'all'
   step = params.getvalue('step') or default_step
   begin = params.getvalue('begin') or default_begin
-  
-  html = '<h1>Statping for ' + host + '</h1>'
-  
-  if not host in get_hosts():
-    return '<p>No such host</p>'
-  
+
   html += '<form method="post" action="' + path + '?host=' + host + '">'
   html += ('<label>Time scale: <select name="step">' +
            '<option value="3600">Last hour</option>' +
@@ -37,16 +58,41 @@ def show_host(params):
            '<option value="604800">Last week</option>' +
            '<option value="18144000">Last month</option>' +
            '<option value="6622560000">Last year</option>' +
-		   '</select></label><br/>')
+           '</select></label><br/>')
   html += ('<label>begin (mm/dd/yyyy hh:ii) : <input type="text" name="begin"'
            'value="' + begin + '"/></label><br/>')
   html += '<input type="submit" value="generate" />'
   html += '</form>'
+  html += '</div>'
+
+  return html
+
+#
+# show_host : list graphs associated with an host
+#
+def show_host(params):
+  html = '<div class="host_graphs">'
+
+  host = params.getvalue('host')
+  step = params.getvalue('step') or default_step
+  begin = params.getvalue('begin') or default_begin
   
-  errors = gen_graph(host, step, begin)
-  if errors != '':
-    html += '<p>Errors: <br/><pre>' + sub('\n', '<br/>', errors) + '</pre></p>'
-  html += '<img src="' + img_path + host + '.png" alt="' + host + '"/>'
+  html += '<h1>Statping for ' + host + '</h1>'
+  
+  if host != 'all' and not host in get_hosts():
+    return '<p>No such host</p>'
+ 
+  if host == 'all':
+    html += '<h1>Statping for all hosts</h1>'
+  else: 
+    html += '<h1>Statping for ' + host + '</h1>'
+    errors = gen_graph(host, step, begin)
+    if errors != '':
+      html += '<p>Errors: <br/><pre>' + sub('\n', '<br/>', errors) + '</pre></p>'
+    html += '<img src="' + img_path + host + '.png" alt="' + host + '"/>'
+
+  html += '</div>'
+
   return html
 
 ### app
@@ -54,16 +100,21 @@ def statping(environ, start_response):
   params = FieldStorage(fp=environ['wsgi.input'], environ=environ)
   start_response('200 OK', [('Content-Type', 'text/html'),
                             ('charset', 'utf-8')])
-  body = ''
-  if admin:
-    body += '<a href="' + path + '?manage=t">manage</a> - '
-  body += '<a href="' + path + '">list</a><br/>'
+  body = '<html>'
+  body += '<head><title>statping</title></head>'
+  body += '<body>'
+
+  body += gen_menu(params)
+  body += gen_renderform(params)
+
   if 'host' in params:
     body += show_host(params)
   elif admin and 'manage' in params or 'new_hosts' in params:
     body += manage_hosts(params)
-  else:
-    body += list_hosts(params)
+
+  body += '</body>'
+  body += '</html>'
+
   return body
 
 ### server
