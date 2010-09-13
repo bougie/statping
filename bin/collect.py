b1843567824849a3434205e23b67628542d2f3aa
Quentin Stievenart <acieroid@awesom.eu>
2010-09-13 18:13:46 +0200

    writing bin/collect.py, small error with sys.path to fix

--- bin/collect.py
+++ bin/collect.py
@@ -1,5 +1,33 @@
 #!/usr/bin/env python
 
-#
-# Collecte data
-#
+import os
+import subprocess
+
+# TODO: import from config
+fping_executable = 'fping'
+
+# parse the response from fping
+def parse_response(string):
+  end_host = string.find(' ')
+  start_delay = string.find(':')+2
+  return (string[0:end_host], float(string[start_delay:]))
+
+# fping a list of hosts, returning a (host, delay) list
+def fping(hosts):
+  pipe = subprocess.Popen([fping_executable, '-C', '1', '-q'] + hosts, 
+                          stdout=subprocess.PIPE,
+                          stderr=subprocess.PIPE)
+  delays = pipe.communicate()[1].splitlines()
+  return map(parse_response, delays)
+
+# collect data
+def collect(hosts):
+  for (host, delay) in fping(hosts):
+    ensure_database_exists(host)
+    add_value(host, delay)
+
+if __name__ == "__main__":
+  import sys
+  sys.path.append(os.path.dirname(__file__) + '/../lib')
+  from lib.get_hosts import get_hosts
+  collect(get_hosts())
